bce9c93eb6bde7276abd2bb27d3595ceb0212513
nishj347 <jainnishpriya@gmail.com>
2024-09-28 11:10:17 +0530

    day1easy

---
+++ day1easy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void scoredteam(int n){
+    if(n>=12){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    scoredteam(n);
+    return 0;
+	
+
+}
